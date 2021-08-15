fd6053efc43ca988fd6f1b6824502cd0163626b2
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-08-15 17:37:41 +0300

    added-basic-homePage

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,11 @@
 import { extendContext } from "../util.js";
+import { getAllHeroes } from "../heroes-service.js";
+import { Barbarian } from "../heroes.js";
 
 export async function homePage(context) {
-    let heroes = getAllHeroes();
+    let pesho = new Barbarian("Pesho");
+    let gosho = new Barbarian("Gosho");
+    let heroes = [pesho, gosho];
     context.heroes = heroes;
     await extendContext(context);
     this.partial('./templates/homePage.hbs', heroes)
--- src/heroes-service.js
+++ src/heroes-service.js
@@ -1,7 +1,14 @@
 const apiKey = 'AIzaSyBgcLHM3nJuJwteuW6NPOs2P0gJ7avANEE';
 const dataBaseUrl = 'https://heroes-application-default-rtdb.europe-west1.firebasedatabase.app/';
+
+const endpoints = {
+    LOGIN: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=`,
+    REGISTER: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=`,
+    HEROES: `heroes`
+};
+
 export async function getAllHeroes() {
-    return request
+    return get(dataBaseUrl + endpoints['HEROES'])
 };
 
 async function request(url, method, body) {
@@ -19,4 +26,20 @@ async function request(url, method, body) {
     let response = await fetch(url, options);
     let data = await response.json();
     return data;
-}
+};
+
+async function get(url) {
+    return request(url, 'GET')
+};
+
+async function post(url) {
+    return request(url, 'POST', body);
+};
+
+async function del(url) {
+    return request(url, 'DELETE');
+};
+
+async function patch(url, body) {
+    return request(url, 'PATCH', body);
+};
