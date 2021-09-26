29af5cfaec41cdcff28bc6699e015842ac450c2c
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-09-26 14:37:22 +0300

    made-logged-user-phone-responsive

--- src/util.js
+++ src/util.js
@@ -59,7 +59,6 @@ export function classToObject(theClass) {
 export async function getAllHeroes() {
     let response = await db.collection('heroes').get();
     let heroes = await response.docs.map((hero) => { return { heroId: hero.id, ...hero.data() } });
-    console.log(heroes);
     return heroes;
 }
 
